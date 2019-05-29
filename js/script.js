2c1d52f57a47ad343dedc52b8619b111acc856ba
Carlos Hernandez <carloshdmn@gmail.com>
2019-05-29 19:17:49 +0200

    Set auto animation for iOS Safari
    To mitigate that device hardware is not accessible

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ var modal = document.getElementById('js-dropdown-modal');
 
 button.addEventListener('click', switchDropdown);
 
+var iosCard = document.getElementById('card-ios');
+var card = document.getElementById('card');
+
+var ua = window.navigator.userAgent;
+var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+var webkit = !!ua.match(/WebKit/i);
+var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
+if (iOSSafari) {
+	card.className += ' hidden';
+} else {
+	iosCard.className += ' hidden';
+}
+
 function switchDropdown() {
 	if (modal.className.indexOf('hidden') > -1) {
 		// Is hidden
